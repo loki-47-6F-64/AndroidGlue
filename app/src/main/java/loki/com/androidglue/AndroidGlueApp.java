98f99d1b3c590ef0f0bfb935677296c90c513da6
loki <loki-47-6F-64@users.noreply.github.com>
2019-03-01 22:35:56 +0100

    update gradle

--- app/src/main/java/loki/com/androidglue/AndroidGlueApp.java
+++ app/src/main/java/loki/com/androidglue/AndroidGlueApp.java
@@ -1,10 +1,20 @@
 package loki.com.androidglue;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+
 import com.loki.superglue.djinni.bluecast.BlueApplication;
+import com.loki.superglue.djinni.bluecast.SuperGlueBlueCast;
 
 public class AndroidGlueApp extends BlueApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        SuperGlueBlueCast.getBluetooth()
+                .setOnBeaconNotificationTap(PendingIntent.getActivity(this, 0, intent, 0));
     }
 }
